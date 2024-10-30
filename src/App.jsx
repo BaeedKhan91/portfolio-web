4b21c8f136e30a8ff795af3230a64cbd4984d151
Baeed khan <baeedk7@gmail.com>
2024-10-30 23:00:35 +0500

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import React ,{ useState } from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Nav from './component/navbar/nav'
-import Eduaction from './pages/eduaction'
-import Home from './pages/home'
-import Contact from './pages/contact'
-import Service from './pages/service'
-import './App.css'
+import Navbar from './component/Navbar/Navbar'
 function App() {
 
 
   return (
     <>
-    <BrowserRouter>
-    <Nav/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/eduaction" element={<Eduaction/>} />
-      <Route path="/contact" element={<Contact/>} />
-      <Route path="/service" element={<Service/>} />
-      
-    </Routes>
-    
-    </BrowserRouter>
-{/* <Home/> */}
+    <Navbar/>
     </>
   )
 }
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../../assets/main-logo.jpg'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <div className='navbar'>
+        
+       
+        <img src={logo} alt="" width='109' height="57"  
+            
+    />
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className="nav-connect">Connect With Me</div>
+        </div>
+  )
+}
+
+export default Navbar
